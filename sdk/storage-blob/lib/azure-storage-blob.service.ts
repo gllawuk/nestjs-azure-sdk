1edad0d168f554520c11259f63761f02fca549e1
Julian Dawson <github@juliandawson.co.uk>
2022-03-11 00:25:17 +0000

    feat(storage-blob): Add new methods

--- sdk/storage-blob/lib/azure-storage-blob.service.ts
+++ sdk/storage-blob/lib/azure-storage-blob.service.ts
@@ -1,9 +1,16 @@
 import { Inject, Injectable } from '@nestjs/common';
 import {
+  BlockBlobParallelUploadOptions,
   BlobItem,
+  BlobGetPropertiesOptions,
   BlobServiceClient,
+  BlockBlobUploadOptions,
+  ContainerItem,
   ContainerListBlobsOptions,
+  ServiceListContainersOptions,
   StorageSharedKeyCredential,
+  BlobDownloadOptions,
+  HttpRequestBody,
 } from '@azure/storage-blob';
 
 import { AzureStorageBlobOptions } from './azure-storage-blob.interface';
@@ -32,6 +39,16 @@ export class AzureStorageBlobService {
     );
   }
 
+  async listContainers(options?: ServiceListContainersOptions) {
+    const list: ContainerItem[] = [];
+    for await (const container of this.blobServiceClient.listContainers(
+      options,
+    )) {
+      list.push(container);
+    }
+    return list;
+  }
+
   async listBlobsFlat(
     containerName: string,
     options?: ContainerListBlobsOptions,
@@ -43,7 +60,78 @@ export class AzureStorageBlobService {
     for await (const blob of containerClient.listBlobsFlat(options)) {
       list.push(blob);
     }
-
     return list;
+  }
+
+  async getBlobProperties(
+    containerName: string,
+    blobName: string,
+    options?: BlobGetPropertiesOptions,
+  ) {
+    const containerClient =
+      this.blobServiceClient.getContainerClient(containerName);
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+    return await blockBlobClient.getProperties(options);
+  }
+
+  async upload(
+    containerName: string,
+    blobName: string,
+    body: HttpRequestBody,
+    contentLength: number,
+    options?: BlockBlobUploadOptions,
+  ) {
+    const containerClient =
+      this.blobServiceClient.getContainerClient(containerName);
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    return await blockBlobClient.upload(body, contentLength, options);
+  }
+
+  async uploadData(
+    containerName: string,
+    blobName: string,
+    data: Buffer | Blob | ArrayBuffer | ArrayBufferView,
+    options?: BlockBlobParallelUploadOptions,
+  ) {
+    const containerClient =
+      this.blobServiceClient.getContainerClient(containerName);
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    return await blockBlobClient.uploadData(data, options);
+  }
+
+  async download(
+    containerName: string,
+    blobName: string,
+    offset?: number,
+    count?: number,
+    options?: BlobDownloadOptions,
+  ) {
+    const containerClient =
+      this.blobServiceClient.getContainerClient(containerName);
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    const downloadBlockBlobResponse = await blockBlobClient.download(
+      offset,
+      count,
+      options,
+    );
+    return await this.streamToBuffer(
+      downloadBlockBlobResponse.readableStreamBody,
+    );
+  }
+
+  private streamToBuffer(
+    readableStream: NodeJS.ReadableStream,
+  ): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+      const chunks: Buffer[] = [];
+      readableStream.on('data', (data: Buffer | string) => {
+        chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+      });
+      readableStream.on('end', () => {
+        resolve(Buffer.concat(chunks));
+      });
+      readableStream.on('error', reject);
+    });
   }
 }
